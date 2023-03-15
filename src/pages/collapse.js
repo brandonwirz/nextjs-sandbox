f32d34146a9f469d374ce66c4c08e1f24b2e6a3d
Brandon Wirz <brandonwirz@Admins-MacBook-Pro.local>
2023-03-15 16:49:57 -0600

    adjusting bootstrap responsiveness

--- src/pages/collapse.js
+++ src/pages/collapse.js
@@ -11,13 +11,15 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 function Example() {
   const [open, setOpen] = useState(false);
   //  <FontAwesomeIcon icon="fa-solid fa-chevron-down" />;
-  const isActive = "Active";
 
+  function openMenu() {
+    setOpen(!open);
+  }
   return (
     <Container>
       <Form>
         <Row className="align-items-center">
-          <Col sm={4} className="my-1">
+          <Col sm={12} lg={4} className="my-1">
             <Form.Label htmlFor="search">User Name</Form.Label>
             <Form.Label htmlFor="search" visuallyHidden>
               Search
@@ -31,10 +33,16 @@ function Example() {
             </InputGroup>
           </Col>
 
-          <Col sm={2} className="my-1">
+          <Col sm lg={2} className="my-1">
             <Form.Label htmlFor="type">Select Type</Form.Label>
             <Form.Label htmlFor="type" visuallyHidden></Form.Label>
-            <Form.Control id="name" placeholder="Search" />
+
+            <br />
+            <Form.Select id="type">
+              <option>Type 1</option>
+              <option>Type 2</option>
+              <option>Type 3</option>
+            </Form.Select>
           </Col>
 
           <Col sm={2} className="my-1">
@@ -58,13 +66,15 @@ function Example() {
             <Form.Control id="phone" placeholder="Search" />
           </Col>
           <Col sm={2}>
-            <Form.Label htmlFor="advanced-search">Advanced Search</Form.Label>
+            <Form.Label id="a-search" htmlFor="advanced-search">
+              Advanced Search
+            </Form.Label>
 
             <Button
               variant="outline-secondary"
-              className="btnclick"
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
+              className="advanced-search"
+              onClick={openMenu}
+              aria-controls="example-collapse-form"
               aria-expanded={open}
               id="advanced-search"
             >
@@ -74,7 +84,11 @@ function Example() {
         </Row>
 
         <br />
-        <Collapse in={open} className="margin-top-20px">
+        <Collapse
+          in={open}
+          id="example-collapse-form"
+          className="margin-top-20px"
+        >
           <Row>
             <Col sm={2}>
               <Form.Label htmlFor="phone">City/State</Form.Label>
@@ -116,15 +130,19 @@ function Example() {
               <Form.Control id="Last-Modified" placeholder="Last Modified" />
             </Col>
             <Col sm={2}>
-              <Form.Label htmlFor="custom-field">Last Modified</Form.Label>
+              <Form.Label htmlFor="custom-field">Active</Form.Label>
               <Form.Label htmlFor="custom-field" visuallyHidden></Form.Label>
-              <Form.Control id="custom-field" placeholder="Last Modified" />
+              <Form.Select id="type">
+                <option>Active 1</option>
+                <option>Active 2</option>
+                <option>Not Active</option>
+              </Form.Select>
             </Col>
 
             <Col sm={2}>
               <Form.Label htmlFor="Active">Active</Form.Label>
 
-              <InputGroup className="mb-3">
+              <InputGroup className="mb-3" id="active-box">
                 <InputGroup.Checkbox aria-label="Checkbox for following text input" />
                 <Form.Control
                   placeholder="Active"
